22c093534a2a6697269ba4a5532874100d8ab0e5
Thomas Weinert <thomas@weinert.info>
2009-06-01 12:11:28 +0000

    FluentDOM: added example for filter($expr)

---
+++ examples/filter-expr.php
@@ -0,0 +1,26 @@
+<?php
+header('Content-type: text/plain');
+
+$xml = <<<XML
+<html>
+<head></head>
+<body>
+  <div></div>
+  <div class="middle"></div>
+  <div class="middle"></div>
+  <div class="middle"></div>
+  <div class="middle"></div>
+  <div></div>
+</body>
+</html>
+XML;
+
+require_once('../FluentDOM.php');
+echo FluentDOM::_($xml)
+  ->find('//div')
+  ->attr('border', 1)
+  ->filter('@class = "middle"')
+  ->attr('style', 'text-align: center;')
+  ->document
+  ->saveXML();
+?>

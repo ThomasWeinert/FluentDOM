4b727830813dfdb58341ab20ae370e3eb92cb9af
Thomas Weinert <thomas@weinert.info>
2009-06-01 11:53:05 +0000

    FluentDOM: added examples directory and map example, removed crappy index.php

---
+++ examples/map.php
@@ -0,0 +1,36 @@
+<?php
+header('Content-type: text/plain');
+
+$xml = <<<XML
+<html>
+<head></head>
+<body>
+  <p><b>Values: </b></p>
+  <form>
+    <input type="text" name="name" value="John"/>
+    <input type="text" name="password" value="password"/>
+    <input type="text" name="url" value="http://ejohn.org/"/>
+  </form>
+</body>
+</html>
+XML;
+
+require_once('../FluentDOM.php');
+$dom = FluentDOM::_($xml);
+echo $dom
+  ->find('//p')
+  ->append(
+    implode(
+      ', ',
+      $dom
+        ->find('//input')
+        ->map(
+          function($node) {
+            return FluentDOM::_($node)->attr('value');
+          }
+        )
+    )
+  )
+  ->document
+  ->saveXML();
+?>

fc9bbd8044bd928afcf324e02b8798cf79ba8c1e
Thomas Weinert <thomas@weinert.info>
2009-06-01 16:29:35 +0000

    FluentDOM: added not() example

---
+++ examples/not.php
@@ -0,0 +1,26 @@
+<?php
+header('Content-type: text/plain');
+
+$xml = <<<XML
+<html>
+<head></head>
+<body>
+  <div></div>
+  <div id="blueone"></div>
+  <div></div>
+  <div class="green"></div>
+  <div class="green"></div>
+  <div class="gray"></div>
+  <div></div>
+</body>
+</html>
+XML;
+
+require_once('../FluentDOM.php');
+echo FluentDOM($xml)
+  ->find('//div')
+  ->not('@class = "green" or @id = "blueone"')
+  ->addClass('blue')
+  ->document
+  ->saveXML();
+?>

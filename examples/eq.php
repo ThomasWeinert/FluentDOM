4d8bce359fe237dffb8cb1b03c6cdd29b077538e
Thomas Weinert <thomas@weinert.info>
2009-06-01 11:59:37 +0000

    FluentDOM: added eq() sample

---
+++ examples/eq.php
@@ -0,0 +1,25 @@
+<?php
+header('Content-type: text/plain');
+
+$xml = <<<XML
+<html>
+<head></head>
+<body>
+  <div></div>
+  <div></div>
+  <div></div>
+  <div></div>
+  <div></div>
+  <div></div>
+</body>
+</html>
+XML;
+
+require_once('../FluentDOM.php');
+echo FluentDOM::_($xml)
+  ->find('//div')
+  ->eq(2)
+  ->addClass('emphased')
+  ->document
+  ->saveXML();
+?>

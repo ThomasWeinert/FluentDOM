7dd71d2db0156920f5455c19707507e52d98d7cc
Thomas Weinert <thomas@weinert.info>
2009-06-04 09:37:24 +0000

    FluentDOM: atom reader sample finished

--- examples/atom/index.php
+++ examples/atom/index.php
@@ -1,8 +1,26 @@
 <!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN">
 <html>
   <head>
-    <title>Atom Reader Sample Script</title>
-    <meta http-equiv="content-type" content="text/html; charset=iso-8859-1">
+    <title>Simple Atom Reader - Sample Script</title>
+    <meta http-equiv="content-type" content="text/html; charset=utf-8">
+    <style type="text/css">
+      body {
+        font-family: sans-serif;
+        background-color: #FFF;
+        color: #000;
+      }
+      a {
+        color: #060;
+      }
+      div.entry {
+        clear: both;
+        padding-top: 2em;
+      }
+      ul.categories li {
+        float: left;
+        margin-left: 20px;
+      }
+    </style>
   </head>
   <body>
     <?php
@@ -11,15 +29,15 @@
       
       $categories = array_unique($dom->find('//_:category')->map('callbackCategoryTerm'));
       if (count($categories) > 0) {
-        echo '<ul class="categories">';
+        echo '<ul class="categories">'."\n";
         foreach ($categories as $category) {
           printf(
-            '<li><a href="?label=%s">%s</a></li>',
+            '<li><a href="?label=%s">%s</a></li>'."\n",
             urlencode($category),
             htmlspecialchars($category)
           );
         }
-        echo '</ul>';
+        echo '</ul>'."\n";
       }
       
       if (empty($_GET['label'])) {
@@ -28,13 +46,38 @@
         $expr = '//_:entry[_:category[@term = "'.htmlspecialchars($_GET['label']).'"]]';
       }
       foreach ($dom->find($expr) as $entryNode) {
+        echo '<div class="entry">'."\n";
         $entry = FluentDOM($entryNode);
         printf(
-          '<h2><a href="?entry=%s&amp;label=%s">%s</a></h2>',
-          htmlspecialchars($entry->find('.//_:id')->text()),
-          htmlspecialchars($label),
+          '<h2><a href="%s">%s</a></h2>'."\n",
+          htmlspecialchars(
+            $entry->find('.//_:link[@rel = "alternate" and @type = "text/html"]')->attr('href')
+          ),
           htmlspecialchars($entry->find('.//_:title')->text())
         );
+        $summary = $entry->find('.//_:summary|.//_:content');
+        switch ($summary->attr('type')) {
+        case 'html' :
+        case 'text/html' :
+          //in real world you whould need to use a purifier
+          printf(
+            '<div class="summary">%s</div>'."\n",
+            $summary->text()
+          );
+          break;
+        case 'text' :
+        case 'text/plain' :
+        case '' :
+          printf(
+            '<div class="summary">%s</div>'."\n",
+            htmlspecialchars($summary->text())
+          );
+          break;
+        default :
+        
+          break;
+        }
+        echo '</div>'."\n";
       }
     ?>
   </body>

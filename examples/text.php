19b6cb092f42b77dcf4d086ffda333a0dfd4693a
Bastian Feder <github@bastian-feder.de>
2009-06-13 11:40:04 +0000

    FleuntDOM: - Implemented: added example for text()

---
+++ examples/text.php
@@ -0,0 +1,53 @@
+<?php
+/**
+*
+* @version $Id $
+* @license http://www.opensource.org/licenses/mit-license.php The MIT License
+* @copyright Copyright (c) 2009 Bastian Feder, Thomas Weinert
+*/
+header('Content-type: text/plain');
+
+$xml = <<<XML
+<html>
+<head></head>
+<body>
+  <div>
+    <p>Hello</p>
+    <p>cruel</p>
+    <p>World!</p>
+  </div>
+</body>
+</html>
+XML;
+
+require_once('../FluentDOM.php');
+
+/*
+ * replace text content of 2nd paragraph
+ */
+echo FluentDOM($xml)
+  ->find('//p[position() = 2]')
+  ->text('nice');
+
+echo "\n\n";
+
+/*
+ * replace text content of every paragraph
+ */
+echo FluentDOM($xml)
+  ->find('//p')
+  ->text('nice');
+
+echo "\n\n";
+
+/*
+ * This explict example is to verify a PHP 'malloc' error thrown on MacOsX 10.5.7
+ * when running test with PHPUnit3.3 - unfortunately it won't appear here
+ *
+ * replace text content of 2nd paragraph
+ */
+echo FluentDOM($xml)
+  ->find('//p[position() = 1]')
+  ->nextSiblings()
+  ->text('nice');
+?>

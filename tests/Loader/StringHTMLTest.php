3625539c4ee3dc50b25e1c8a6b0c974379b60eb6
Bastian Feder <github@bastian-feder.de>
2009-07-12 19:33:33 +0000

    FluentDOMTest:
    - Implemented: added test case for loader StringHTML

---
+++ tests/Loader/StringHTMLTest.php
@@ -0,0 +1,43 @@
+<?php
+/**
+* HTML string loader test for FluentDOM
+*
+* @version $Id$
+* @license http://www.opensource.org/licenses/mit-license.php The MIT License
+* @copyright Copyright (c) 2009 Bastian Feder, Thomas Weinert
+*
+* @package FluentDOM
+* @subpackage unitTests
+*/
+
+/**
+* load necessary files
+*/
+require_once 'PHPUnit/Framework.php';
+require_once dirname(__FILE__).'/../../Loader/StringHTML.php';
+
+PHPUnit_Util_Filter::addFileToFilter(__FILE__);
+
+/**
+* Test class for FluentDOMLoaderStringHTML.
+*
+* @package FluentDOM
+* @subpackage unitTests
+*/
+class FluentDOMLoaderStringHTMLTest extends PHPUnit_Framework_TestCase {
+
+  public function testLoad() {
+    $loader = new FluentDOMLoaderStringHTML();
+    $fd = $loader->load('<html><body></body></html>', 'html');
+    $this->assertTrue($fd instanceof DOMDocument);
+    $this->assertEquals('html', $fd->documentElement->nodeName);
+  }
+
+  public function testLoadInvalid() {
+    $loader = new FluentDOMLoaderStringHTML();
+    $result = $loader->load('<html><body></body></html>', 'html');
+    $this->assertFalse($result);
+  }
+}
+
+?>

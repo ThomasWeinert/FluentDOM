4e6e0b1d38e15587c5e58b3768b6b1be13d356e6
Bastian Feder <github@bastian-feder.de>
2009-07-12 21:02:58 +0000

    FluentDOMLoader:
    - Implemented: added exception if passed string is not a file

--- Loader/FileXML.php
+++ Loader/FileXML.php
@@ -19,11 +19,16 @@ require_once dirname(__FILE__).'/../FluentDOMLoader.php';
 * Load FluentDOM from XML file
 */
 class FluentDOMLoaderFileXML implements FluentDOMLoader {
-  
+
   public function load($source, $contentType) {
     if (is_string($source) &&
         FALSE === strpos($source, '<') &&
         in_array($contentType, array('xml', 'text/xml'))) {
+
+      if (!file_exists($source)) {
+        throw new InvalidArgumentException('File not found: '. $source);
+      }
+
       $dom = new DOMDocument();
       $dom->load($source);
       return $dom;

f22f59ccc347753af45f0f1f864529088005bce0
Bastian Feder <github@bastian-feder.de>
2009-07-12 15:04:40 +0000

    FluentDOMTest:
    - Implemented: base methods for fixture setup

--- tests/FluentDOMTest.php
+++ tests/FluentDOMTest.php
@@ -56,10 +56,55 @@ class FluentDOMTest extends PHPUnit_Framework_TestCase {
     $this->_directory = dirname(__FILE__);
   }
 
+  /**
+  * Tests, if the content of a file equals the given string
+  *
+  * The the file to be compared is identified by the given function name.
+  *
+  * @param string $functionName
+  * @param string $actual
+  *
+  * @uses getFileName()
+  */
+  protected function assertFluentDOMEqualsXMLFile($functionName, $actual) {
+    $fileName = $this->getFileName($functionName, 'tgt');
+    $this->assertXmlStringEqualsXmlFile($fileName, $actual);
+  }
+
+  /**
+  *
+  * @param string $functionName
+  * @return FluentDOM
+  */
+  protected function getFixtureFromFile($functionName) {
+    $fileName = $this->getFileName($functionName, 'src');
+    if (!file_exists($fileName)) {
+      throw new Exception('File Not Found: '. $fileName);
+    }
+
+    $fd = new FluentDOM();
+    return $fd->load($fileName);
+  }
+
+  /**
+  *
+  * @param string $functionName
+  * @param string $type
+  * @return string
+  */
+  protected function getFileName($functionName, $type) {
+    return sprintf('%s/data/%s%s.%s.xml',
+      $this->_directory,
+      strToLower(substr($functionName, 4, 1)),
+      substr($functionName, 5),
+      $type
+    );
+  }
+
   /*
   * Load
   */
-  
+
   /**
   * @group Load
   */
@@ -1093,12 +1138,11 @@ class FluentDOMTest extends PHPUnit_Framework_TestCase {
   * @group Manipulation
   */
   function testAppend() {
-    $this->assertFileExists($this->_directory.'/data/append.src.xml');
-    $doc = FluentDOM($this->_directory.'/data/append.src.xml')
-      ->find('//p')
+    $fd = $this->getFixtureFromFile(__FUNCTION__);
+    $fd->find('//p')
       ->append('<strong>Hello</strong>');
-    $this->assertTrue($doc instanceof FluentDOM);
-    $this->assertXmlStringEqualsXMLFile($this->_directory.'/data/append.tgt.xml', $doc);
+    $this->assertTrue($fd instanceof FluentDOM);
+    $this->assertFluentDOMEqualsXMLFile(__FUNCTION__, $fd);
   }
 
   /**

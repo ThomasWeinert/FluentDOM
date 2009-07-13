ad1f0e25da1be1a4f7a196bf0f693040708118f6
Thomas Weinert <thomas@weinert.info>
2009-07-13 11:01:53 +0000

    FluentDOM examples: added sample for custom loader (FluentDOMIniLoader)

---
+++ examples/iniloader/FluentDOMIniLoader.php
@@ -0,0 +1,57 @@
+<?php
+
+require_once dirname(__FILE__).'/../../FluentDOMLoader.php';
+
+class FluentDOMIniLoader implements FluentDOMLoader {
+
+  public function load($source, $contentType) {
+    if (is_string($source) &&
+        in_array($contentType, array('ini', 'text/ini'))) {
+        
+      if (!file_exists($source)) {
+        throw new InvalidArgumentException('File not found: '. $source);
+      }
+      
+      if ($iniFile = parse_ini_file($source)) {
+        $dom = new DOMDocument();
+        $root = $dom->appendChild($dom->createElement('ini'));
+        $this->_arrayToNodes($dom, $root, $iniFile);
+        return $dom;
+      }
+    }
+    return FALSE;
+  }
+  
+  protected function _arrayToNodes(&$dom, &$node, $data) {
+    if (is_array($data)) {
+      foreach ($data as $key => $val) {
+        if (preg_match('(^\d+$)', $key)) {
+          $nodeName = $node->nodeName;
+          if (substr($nodeName, -1) == 's') {
+            $nodeName = substr($nodeName, 0, -1);
+          }
+          $childNode = $dom->createElement($nodeName);
+          $this->_arrayToNodes($dom, $childNode, $val);
+          $node->appendChild($childNode);
+        } elseif (is_array($val)) {
+          $childNode = $dom->createElement($key);
+          $this->_arrayToNodes($dom, $childNode, $val);
+          $node->appendChild($childNode);
+        } elseif (preg_match('([\r\n\t])', $val)) {
+          $childNode = $dom->createElement($key);
+          $textNode = $dom->createTextNode($val);
+          $childNode->appendChild($textNode);
+          $node->appendChild($childNode);
+        } else {
+          $node->appendChild($dom->createElement($key, $val));
+        }
+      }
+    } elseif (!empty($data)) {
+      $textNode = $dom->createTextNode($data);
+      $node->appendChild($textNode);
+    }
+  }
+  
+}
+
+?>
---
+++ examples/iniloader/iniToXml.php
@@ -0,0 +1,28 @@
+<?php
+/**
+* Sample how to use a custom FluentDOMLoader
+* 
+* @version $Id$
+* @package FluentDOM
+* @subpackage examples
+*/
+
+require_once dirname(__FILE__).'/../../FluentDOM.php';
+require_once dirname(__FILE__).'/FluentDOMIniLoader.php';
+
+$iniFile = dirname(__FILE__).'/sample.ini';
+
+$fd = new FluentDOM();
+$fd->setLoaders(
+  array(
+    new FluentDOMIniLoader()
+  )
+);
+
+header('Content-type: text/plain');
+echo $fd->load($iniFile, 'text/ini')->formatOutput();
+
+echo "\n\n";
+echo 'URL: ', $fd->find('//URL')->text();
+
+?>

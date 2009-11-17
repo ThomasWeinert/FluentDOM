b4892cc4d7bf865f427b08b47a06211159480b9e
Thomas Weinert <thomas@weinert.info>
2009-11-17 20:14:35 +0000

    - Documented: optimized structure for html output - Documented: custom loaders

---
+++ examples/basic-usage/load-custom.php
@@ -0,0 +1,28 @@
+<?php
+
+require('../../FluentDOM.php');
+
+$xmlFile = 'hello.xml';
+
+class ExampleLoader implements FluentDOMLoader {
+  // this could implement checks, error handling, ... 
+	public function load($source, $type) {
+  	$dom = new DOMDocument();
+  	$dom->load($source);
+  	return $dom;
+  }	
+}
+
+$fd = new FluentDOM();
+// set loader(s)
+$fd->setLoaders(
+  array(
+    new ExampleLoader
+  )
+);
+// load data  using custom loader
+$fd->load($xmlFile);
+
+echo $fd
+  ->find('/message')
+  ->text('Hello World!');

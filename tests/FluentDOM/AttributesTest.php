1aee0b437dce7595186486ca1fd5e53b7ac0abd0
Thomas Weinert <thomas@weinert.info>
2010-09-12 16:14:02 +0200

    - Tested: FluentDOMattributes::__construct() - Tested: FluentDOMAttributes::toArray()

--- tests/FluentDOM/AttributesTest.php
+++ tests/FluentDOM/AttributesTest.php
@@ -19,4 +19,41 @@ PHPUnit_Util_Filter::addFileToFilter(__FILE__);
 
 class FluentDOMAttributesTest extends FluentDOMTestCase {
 
+  /**
+  * @covers FluentDOMAttributes::__construct
+  */
+  public function testConstructor() {
+    $fd = $this->getMock('FluentDOM');
+    $attr = new FluentDOMAttributes($fd);
+    $this->assertAttributeSame(
+      $fd, '_fd', $attr
+    );
+  }
+
+  /**
+  * @covers FluentDOMAttributes::toArray
+  */
+  public function testToArray() {
+    $dom = new DOMDocument;
+    $node = $dom->createElement('sample');
+    $node->setAttribute('foo', 1);
+    $node->setAttribute('bar', 2);
+    $dom->appendChild($node);
+    $fd = $this->getMock('FluentDOM');
+    $fd
+      ->expects($this->any())
+      ->method('offsetExists')
+      ->with(0)
+      ->will($this->returnValue(TRUE));
+    $fd
+      ->expects($this->any())
+      ->method('offsetGet')
+      ->with(0)
+      ->will($this->returnValue($node));
+    $attr = new FluentDOMAttributes($fd);
+    $this->assertEquals(
+      array('foo' => 1, 'bar' => 2),
+      $attr->toArray()
+    );
+  }
 }

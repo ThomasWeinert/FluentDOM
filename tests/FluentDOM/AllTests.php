4f072666d8ed8ca7b449d1b1d18f54635ba6bf9b
Thomas Weinert <thomas@weinert.info>
2011-02-01 22:30:02 +0100

    Integrated FluentDOMData tests into test suite

--- tests/FluentDOM/AllTests.php
+++ tests/FluentDOM/AllTests.php
@@ -20,6 +20,7 @@ require_once(dirname(__FILE__).'/CoreTest.php');
 if (version_compare(PHP_VERSION, '5.3', '>=')) {
   include_once(dirname(__FILE__).'/CoreTest_PHP5_3.php');
 }
+require_once(dirname(__FILE__).'/DataTest.php');
 require_once(dirname(__FILE__).'/HandlerTest.php');
 require_once(dirname(__FILE__).'/IteratorTest.php');
 require_once(dirname(__FILE__).'/Loader/AllTests.php');
@@ -46,6 +47,7 @@ class FluentDOMClasses_AllTests {
     if (version_compare(PHP_VERSION, '5.3', '>=')) {
       $suite->addTestSuite('FluentDOMCoreTest_PHP5_3');
     }
+    $suite->addTestSuite('FluentDOMDataTest');
     $suite->addTestSuite('FluentDOMHandlerTest');
     $suite->addTestSuite('FluentDOMIteratorTest');
     $suite->addTestSuite('FluentDOMLoader_AllTests');

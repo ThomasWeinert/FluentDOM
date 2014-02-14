dc357027f19bfbe26f8b6090f01e5d25453ec8e4
Thomas Weinert <thomas@weinert.info>
2014-02-15 00:53:39 +0100

    Added FluentDOM::QueryCss(), use FluentDOM with css selectors

---
+++ examples/css-selectors/find.php
@@ -0,0 +1,25 @@
+<?php
+/**
+* @license http://www.opensource.org/licenses/mit-license.php The MIT License
+* @copyright Copyright (c) 2009-2014 Bastian Feder, Thomas Weinert
+*/
+header('Content-type: text/plain');
+
+$xml = <<<XML
+<html>
+  <head>
+    <title>Examples: FluentDOM\Query::find()</title>
+  </head>
+  <body>
+    <p><span class="edit">Hello</span>, how are you?</p>
+    <p>Me? I'm <span>good</span>.</p>
+  </body>
+</html>
+XML;
+
+$loader = require_once('../../vendor/autoload.php');
+
+echo FluentDOM::QueryCss($xml)
+  ->find('p')
+  ->find('span')
+  ->addClass('red');

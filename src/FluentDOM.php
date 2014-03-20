e22a2e4101fb90b98672224c979a59874f18f57c
Thomas Weinert <thomas@weinert.info>
2014-03-20 22:45:10 +0100

    Added the FluentDOM function again for BC and comfort.

--- src/FluentDOM.php
+++ src/FluentDOM.php
@@ -4,6 +4,13 @@ abstract class FluentDOM {
 
   private static $_load = TRUE;
 
+  /**
+   * Create an FluentDOM::Query instance and load the source into it.
+   *
+   * @param mixed $source
+   * @param string $contentType
+   * @return \FluentDOM\Query
+   */
   public static function Query($source = NULL, $contentType = 'text/xml') {
     if (self::$_load && !class_exists('FluentDOM\Query')) {
       // @codeCoverageIgnoreStart
@@ -17,6 +24,14 @@ abstract class FluentDOM {
     return $query;
   }
 
+  /**
+   * Create an FluentDOM::Query instance with a modified selector callback.
+   * This allows to use CSS selectors instead of Xpath expression.
+   *
+   * @param mixed $source
+   * @param string $contentType
+   * @return \FluentDOM\Query
+   */
   public static function QueryCss($source = NULL, $contentType = 'text/xml') {
     if (!class_exists('PhpCss')) {
       throw new \LogicException('Install "carica/phpcss" to support css selectors.');
@@ -27,4 +42,15 @@ abstract class FluentDOM {
     };
     return $query;
   }
+}
+
+/**
+ * FluentDOM function, is an Alias for the FluentDOM::Query()
+ * factory class function.
+ *
+ * @param null $source
+ * @param string $contentType
+ */
+function FluentDOM($source = NULL, $contentType = 'text/xml') {
+  return FluentDOM::Query($source, $contentType);
 }

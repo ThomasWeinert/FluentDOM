db91fd9c52a59465ee01c2e5e87290fc10f370f5
Thomas Weinert <thomas@weinert.info>
2014-06-19 15:47:54 +0200

    Tested: FluentDOM\Query::__isset(), FluentDOM\Query::__unset()

--- tests/FluentDOM/QueryTest.php
+++ tests/FluentDOM/QueryTest.php
@@ -15,5 +15,38 @@ namespace FluentDOM {
       /** @noinspection PhpUndefinedMethodInspection */
       $fd->invalidMethodCall();
     }
+
+    /**
+     * @group Properties
+     * @covers FluentDOM\Query::__isset
+     * @dataProvider providePropertyNames
+     */
+    public function testIssetPropertyContentType($propertyName) {
+      $fd = new Query();
+      $this->assertTrue(isset($fd->$propertyName));
+    }
+
+    /**
+     * @group Properties
+     * @covers FluentDOM\Query::__isset
+     * @dataProvider providePropertyNames
+     */
+    public function testUnsetPropertyContentType($propertyName) {
+      $fd = new Query();
+      $this->setExpectedException(
+        'BadMethodCallException',
+        'Can not unset property FluentDOM\Query::$'.$propertyName
+      );
+      unset($fd->$propertyName);
+    }
+
+    public static function providePropertyNames() {
+      return array(
+        array('attr'),
+        array('css'),
+        array('data'),
+        array('contentType')
+      );
+    }
   }
 }

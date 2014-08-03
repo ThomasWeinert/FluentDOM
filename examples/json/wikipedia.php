258983430fbcf9a3e13dd73ca7a0eb6b6094ffff
Thomas Weinert <thomas@weinert.info>
2014-08-03 14:52:00 +0200

    Added example: reading json

---
+++ examples/json/wikipedia.php
@@ -0,0 +1,29 @@
+<?php
+require_once(dirname(__FILE__).'/../../vendor/autoload.php');
+
+$json = <<<JSON
+{
+  "firstName": "John",
+  "lastName": "Smith",
+  "age": 25,
+  "address": {
+    "streetAddress": "21 2nd Street",
+    "city": "New York",
+    "state": "NY",
+    "postalCode": 10021
+  },
+  "phoneNumbers": [
+    {
+      "type": "home",
+      "number": "212 555-1234"
+    },
+    {
+      "type": "fax",
+      "number": "646 555-4567"
+    }
+  ]
+}
+JSON;
+
+$fd = FluentDOM($json, 'text/json');
+echo $fd->find('/*/phoneNumbers/*[type="home"]/number')->text();

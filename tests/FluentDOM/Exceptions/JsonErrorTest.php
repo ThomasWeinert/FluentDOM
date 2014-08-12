bbe40d0eb9e76c83403bd937b9380877159333c0
Thomas Weinert <weinert@dimensional.de>
2014-08-12 21:39:50 +0200

    Tested: FluentDOM\Exceptions\JsonError

---
+++ tests/FluentDOM/Exceptions/JsonErrorTest.php
@@ -0,0 +1,28 @@
+<?php
+namespace FluentDOM\Exceptions {
+
+  use FluentDOM\TestCase;
+
+  require_once(__DIR__.'/../TestCase.php');
+
+  class JsonErrorTest extends TestCase {
+
+    /**
+     * @covers FluentDOM\Exceptions\JsonError
+     */
+    public function testWithValidErrorCode() {
+      $exception = new JsonError(1);
+      $this->assertEquals(1, $exception->getCode());
+      $this->assertEquals('The maximum stack depth has been exceeded', $exception->getMessage());
+    }
+
+    /**
+     * @covers FluentDOM\Exceptions\JsonError
+     */
+    public function testWithInvalidErrorCodeExpectingUnknonwError() {
+      $exception = new JsonError(-42);
+      $this->assertEquals(-42, $exception->getCode());
+      $this->assertEquals('Unknown error has occurred', $exception->getMessage());
+    }
+  }
+}

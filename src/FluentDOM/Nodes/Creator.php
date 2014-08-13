1ad87f3e95919e2c960438e3d2e3fd435595fc11
Thomas Weinert <thomas@weinert.info>
2014-08-14 01:50:30 +0200

    Added phpdoc annotations

--- src/FluentDOM/Nodes/Creator.php
+++ src/FluentDOM/Nodes/Creator.php
@@ -5,28 +5,51 @@ namespace FluentDOM\Nodes {
   use FluentDOM\Appendable;
   use FluentDOM\Document;
 
+  /**
+   * @property bool $formatOutput
+   */
   class Creator {
 
-    private $_document = '';
+    /**
+     * @var Document
+     */
+    private $_document = NULL;
 
+    /**
+     * @param Document $document
+     */
     public function __construct(Document $document = NULL) {
       $this->_document = $document ?: new Document();
     }
 
+    /**
+     * @param string $name
+     * @return bool
+     */
     public function __isset($name) {
       switch ($name) {
       case 'formatOutput' :
         return isset($this->_document->{$name});
       }
+      return FALSE;
     }
 
+    /**
+     * @param string $name
+     * @return mixed
+     */
     public function __get($name) {
       switch ($name) {
       case 'formatOutput' :
         return $this->_document->{$name};
       }
+      return NULL;
     }
 
+    /**
+     * @param $name
+     * @param $value
+     */
     public function __set($name, $value) {
       switch ($name) {
       case 'formatOutput' :
@@ -34,10 +57,19 @@ namespace FluentDOM\Nodes {
       }
     }
 
+    /**
+     * @param string $prefix
+     * @param string $namespaceUri
+     */
     public function registerNamespace($prefix, $namespaceUri) {
       $this->_document->registerNamespace($prefix, $namespaceUri);
     }
 
+    /**
+     * @param string $name
+     * @param mixed ...$parameter
+     * @return Creator\Node
+     */
     public function __invoke($name) {
       $node = $this->_document->createElement($name);
       $arguments = func_get_args();
@@ -66,16 +98,38 @@ namespace FluentDOM\Nodes {
 
 namespace FluentDOM\Nodes\Creator {
 
+  use FluentDOM\Document;
+
+  /**
+   * @property-read Document $document
+   * @property-read Document $dom
+   * @property-read \DOMElement $node
+   */
   class Node {
 
+    /**
+     * @var Document
+     */
     private $_document = NULL;
+
+    /**
+     * @var \DOMElement
+     */
     private $_node = NULL;
 
-    public function __construct($document, \DOMNode $node) {
+    /**
+     * @param Document $document
+     * @param \DOMElement $node
+     */
+    public function __construct($document, \DOMElement $node) {
       $this->_document = $document;
       $this->_node = $node;
     }
 
+    /**
+     * @param string $name
+     * @return mixed
+     */
     public function __get($name) {
       switch ($name) {
       case 'dom' :
@@ -84,20 +138,31 @@ namespace FluentDOM\Nodes\Creator {
       case 'node' :
         return $this->_node;
       }
+      return NULL;
     }
 
+    /**
+     * @param string $name
+     * @param mixed $value
+     */
     public function __set($name, $value) {
       throw new \LogicException(
         sprintf('%s is immutable.', get_class($this))
       );
     }
 
+    /**
+     * @return Document
+     */
     public function getDocument() {
       $document = clone $this->_document;
       $document->appendChild($document->importNode($this->_node, TRUE));
       return $document;
     }
 
+    /**
+     * @return string
+     */
     public function __toString() {
       return $this->getDocument()->saveXml() ?: '';
     }

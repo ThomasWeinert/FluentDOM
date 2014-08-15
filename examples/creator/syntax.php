651802bd9a2407a0488dfd7d6b99d69b72a82d36
Thomas Weinert <thomas@weinert.info>
2014-08-15 09:20:13 +0200

    Added creator syntax example

---
+++ examples/creator/syntax.php
@@ -0,0 +1,35 @@
+<?php
+
+require_once(dirname(__FILE__).'/../../vendor/autoload.php');
+
+$_ = FluentDOM::create();
+$_->formatOutput = TRUE;
+
+echo $_(
+  'root',
+  $_('child', 'text', ['attr' => 'value']),
+  $_(
+    'traversable-mapping',
+    $_->any(
+      ['one', 'two'],
+      function($text, $index) use ($_) {
+        return $_('item', $text, ['index' => $index]);
+      }
+    )
+  ),
+  $_(
+    'node-types',
+    $_(
+      'cdata',
+      $_->cdata(' cdata section')
+    ),
+    $_(
+      'comment',
+      $_->comment('comment text')
+    ),
+    $_(
+      'processing-instruction',
+      $_->pi('php', 'echo "Hello World!";')
+    )
+  )
+);

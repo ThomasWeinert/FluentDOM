df51a0736ce402dad8f60297a0162d36cb13067c
Thomas Weinert <thomas@weinert.info>
2014-08-16 16:32:59 +0200

    Correct file contents.

--- src/FluentDOM/Serializer/Json/Rayfish.php
+++ src/FluentDOM/Serializer/Json/Rayfish.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * Serialize a DOM to JsonML: http://www.jsonml.org/
+ * Serialize a DOM to Rayfish Json: http://www.bramstein.com/projects/xsltjson/
  *
  * @license http://www.opensource.org/licenses/mit-license.php The MIT License
  * @copyright Copyright (c) 2009-2014 Bastian Feder, Thomas Weinert
@@ -9,22 +9,44 @@
 namespace FluentDOM\Serializer\Json {
 
   use FluentDOM\Serializer\Json;
-  use FluentDOM\Xpath;
 
   /**
-   * Serialize a DOM to JsonML: http://www.jsonml.org/
+   * Serialize a DOM to Rayfish Json: http://www.bramstein.com/projects/xsltjson/
    *
    * @license http://www.opensource.org/licenses/mit-license.php The MIT License
    * @copyright Copyright (c) 2009-2014 Bastian Feder, Thomas Weinert
    */
-  class JsonML extends Json {
+  class Rayfish extends Json {
 
     /**
-     * @return array|NULL
+     * @param \DOMElement $node
+     * @return array
      */
-    public function jsonSerialize() {
-      if (isset($this->_document->documentElement)) {
-        return $this->getNode($this->_document->documentElement);
+    protected function getNode(\DOMElement $node) {
+      if ($node instanceof \DOMElement) {
+        $result = new \stdClass();
+        $result->{'#name'} = $node->nodeName;
+        $result->{'#text'} = '';
+        $result->{'#children'} = array_merge(
+          $this->getAttributes($node)
+        );
+        foreach ($node->childNodes as $childNode) {
+          if ($childNode instanceof \DOMElement) {
+            $result->{'#children'}[] = $this->getNode($childNode);
+          } elseif (
+            (
+              $childNode instanceof \DOMText ||
+              $childNode instanceof \DOMCdataSection
+            ) &&
+            !$childNode->isWhitespaceInElementContent()
+          ) {
+            $result->{'#text'} .= $childNode->textContent;
+          }
+        }
+        if (empty($result->{'#text'})) {
+          $result->{'#text'} = NULL;
+        }
+        return $result;
       }
       return NULL;
     }
@@ -33,81 +55,16 @@ namespace FluentDOM\Serializer\Json {
      * @param \DOMElement $node
      * @return array
      */
-    private function getNode(\DOMElement $node) {
-      $result = [
-        $node->nodeName
-      ];
-      $attributes = array_merge(
-        $this->getNamespaces($node),
-        $this->getAttributes($node)
-      );
-      if (!empty($attributes)) {
-        $result[] = $attributes;
-      }
-      foreach ($node->childNodes as $childNode) {
-        if ($childNode instanceof \DOMElement) {
-          $result[] = $this->getNode($childNode);
-        } elseif (
-          $childNode instanceof \DOMText ||
-          $childNode instanceof \DOMCdataSection
-        ) {
-          $result[] = $this->getValue($childNode->data);
-        }
-      }
-      return $result;
-    }
-
-    /**
-     * @param \DOMElement $node
-     * @return array|NULL
-     */
     private function getAttributes(\DOMElement $node) {
       $result = [];
-      foreach ($node->attributes as $name => $attribute) {
-        $result[$name] = $this->getValue($attribute->value);
+      foreach ($node->attributes as $name => $attributeNode) {
+        $attribute = new \stdClass();
+        $attribute->{'#name'} = '@'.$attributeNode->name;
+        $attribute->{'#text'} = $attributeNode->value;
+        $attribute->{'#children'} = [];
+        $result[] = $attribute;
       }
       return $result;
-    }
-
-    /**
-     * @param \DOMElement $node
-     * @return array
-     */
-    private function getNamespaces(\DOMElement $node) {
-      $result = [];
-      $xpath = new Xpath($node->ownerDocument);
-      if (($uri = $node->getAttribute('xmlns')) !== '') {
-        $result['xmlns'] = $uri;
-      }
-      foreach ($xpath->evaluate('namespace::*', $node) as $namespace) {
-        if (($uri = $node->getAttribute($namespace->nodeName)) !== '') {
-          $result[$namespace->nodeName] = $uri;
-        }
-      }
-      return $result;
-    }
-
-    private function getValue($value) {
-      if ($this->isBoolean($value)) {
-        return (strtolower($value) === 'true');
-      } elseif ($this->isInteger($value)) {
-        return (int)$value;
-      } elseif ($this->isNumber($value)) {
-        return (float)$value;
-      }
-      return $value;
-    }
-
-    private function isInteger($value) {
-      return (bool)preg_match('(^[1-9]\d*$)D', $value);
-    }
-
-    private function isNumber($value) {
-      return (bool)preg_match('(^(?:\\d+\\.\\d+|[1-9]\d*)$)D', $value);
-    }
-
-    private function isBoolean($value) {
-      return (bool)preg_match('(^(?:true|false)$)Di', $value);
     }
   }
 }

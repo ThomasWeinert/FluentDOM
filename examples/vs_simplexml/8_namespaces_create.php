1ab63d30288a078d785375e11906f576861d381d
Thomas Weinert <thomas@weinert.info>
2014-10-04 14:40:24 +0200

    Added example creating xml with namespaces using SimpleXML and FluentDOM

---
+++ examples/vs_simplexml/8_namespaces_create.php
@@ -0,0 +1,22 @@
+<?php
+require(dirname(__FILE__).'/../../vendor/autoload.php');
+
+$element = simplexml_load_string('<atom:feed xmlns:atom="http://www.w3.org/2005/Atom"/>');
+$title = $element->addChild('atom:title', 'http://www.w3.org/2005/Atom');
+$title->{0} = 'FluentDOM Feed';
+
+echo $element->saveXml();
+
+$document = new FluentDOM\Document();
+$document->registerNamespace('atom', 'http://www.w3.org/2005/Atom');
+$document
+  ->appendElement('atom:feed')
+  ->appendElement('atom:title', 'FluentDOM Feed');
+echo $document->saveXml();
+
+$_ = FluentDOM::create();
+$_->registerNamespace('atom', 'http://www.w3.org/2005/Atom');
+echo $_(
+  'atom:feed',
+  $_('atom:title', 'FluentDOM Feed')
+);

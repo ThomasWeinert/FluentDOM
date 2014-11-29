dc162ab3696fa5f8e28249fa8c40253d8a093b60
Thomas Weinert <thomas@weinert.info>
2014-11-29 15:36:02 +0100

    Added new loader for text/calendar (converts icalendar to xcalendar)

--- examples/icalendar/xcalendar.php
+++ examples/icalendar/xcalendar.php
@@ -4,4 +4,7 @@ require_once(__DIR__.'/../../vendor/autoload.php');
 
 $dom = FluentDOM::load(__DIR__.'/example.ical', 'text/calendar');
 $dom->formatOutput = TRUE;
+
+echo $dom('string(//xCal:VEVENT/xCal:SUMMARY)');
+echo "\n\n";
 echo $dom->saveXml();

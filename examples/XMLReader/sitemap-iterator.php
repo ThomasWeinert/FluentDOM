2deda1d200bd7322a213545bbd164efa1f65369d
Thomas Weinert <thomas@weinert.info>
2017-07-17 20:27:47 +0200

    Split examples

--- examples/XMLReader/sitemap-iterator.php
+++ examples/XMLReader/sitemap-iterator.php
@@ -25,16 +25,13 @@ $reader->open('data://text/plain;base64,'.base64_encode($xml));
 $reader->registerNamespace('s', 'http://www.sitemaps.org/schemas/sitemap/0.9');
 $reader->registerNamespace('v', 'http://www.google.com/schemas/sitemap-video/1.1');
 
-if ($reader->read('s:url')) {
-  do {
-    /** @var FALSE|FluentDOM\Element $url */
-    $url = $reader->expand();
-    var_dump(
-      [
-        $url('string(v:video/v:title)'),
-        $url('string(s:loc)')
-      ]
-    );
-  } while ($reader->next('s:url'));
+foreach (new FluentDOM\XMLReader\SiblingIterator($reader, 's:url') as $url) {
+  /** @var FALSE|FluentDOM\Element $url */
+  var_dump(
+    [
+      $url('string(v:video/v:title)'),
+      $url('string(s:loc)')
+    ]
+  );
 }
 

0e44e81776d32235524bd639cccb5f0ec2a4d99c
Thomas Weinert <thomas@weinert.info>
2017-07-27 15:24:02 +0200

    Tested: FluentDOM\XMLWriter\NamespaceDefinition

---
+++ tests/FluentDOM/XMLWriter/NamespaceDefinitionTest.php
@@ -0,0 +1,19 @@
+<?php
+namespace FluentDOM\XMLWriter {
+
+  use FluentDOM\TestCase;
+
+  require_once(__DIR__ . '/../TestCase.php');
+
+  class NamespaceDefinitionTest extends TestCase {
+
+    /**
+     * @covers \FluentDOM\XMLWriter\NamespaceDefinition
+     */
+    public function testDecreaseExpectingException() {
+      $definition = new NamespaceDefinition();
+      $this->expectException(\LogicException::class);
+      $definition->decreaseDepth();
+    }
+  }
+}

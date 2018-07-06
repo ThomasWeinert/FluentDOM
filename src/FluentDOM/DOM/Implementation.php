ad94d558fa80243bb7bf97164ee6c3731ca3a7e8
Thomas Weinert <thomas@weinert.info>
2018-07-07 00:12:43 +0200

    Add extended DOMImplementation

---
+++ src/FluentDOM/DOM/Implementation.php
@@ -0,0 +1,35 @@
+<?php
+/**
+ * FluentDOM
+ *
+ * @link https://thomas.weinert.info/FluentDOM/
+ * @copyright Copyright 2009-2018 FluentDOM Contributors
+ * @license http://www.opensource.org/licenses/mit-license.php The MIT License
+ *
+ */
+
+namespace FluentDOM\DOM {
+
+  use DOMDocumentType;
+
+  /**
+   * Extend DOMImplementation to return FluentDOM\DOM classes
+   */
+  class Implementation extends \DOMImplementation {
+
+    public function createDocument($namespaceURI = NULL, $qualifiedName = NULL, DOMDocumentType $doctype = NULL) {
+      $document = new Document();
+      if ($doctype) {
+        $document->appendChild($doctype);
+      }
+      if ($qualifiedName) {
+        $document->appendElement($document->createElementNS($namespaceURI, $qualifiedName));
+        $prefix = strstr($qualifiedName, ':', TRUE);
+        if ($prefix !== '' || !empty($namespaceURI)) {
+          $document->registerNamespace('', $namespaceURI);
+        }
+      }
+      return $document;
+    }
+  }
+}

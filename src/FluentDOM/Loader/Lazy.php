478080d1270a040a232ca89af3b2a61d58092e08
Thomas Weinert <thomas@weinert.info>
2019-01-25 21:29:20 +0100

    Fix phpdoc type annotation

--- src/FluentDOM/Loader/Lazy.php
+++ src/FluentDOM/Loader/Lazy.php
@@ -3,7 +3,7 @@
  * FluentDOM
  *
  * @link https://thomas.weinert.info/FluentDOM/
- * @copyright Copyright 2009-2018 FluentDOM Contributors
+ * @copyright Copyright 2009-2019 FluentDOM Contributors
  * @license http://www.opensource.org/licenses/mit-license.php The MIT License
  *
  */
@@ -115,7 +115,7 @@ namespace FluentDOM\Loader {
 
     /**
      * @param mixed $source
-     * @param mixed $contentType
+     * @param string $contentType
      * @param array|\Traversable|Options $options
      * @return Document|Result|NULL
      */
